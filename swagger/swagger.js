6b0ad42d2c1ddbb6ee1899abb2177e2c05821cfc
McKay <wmsmckay@gmail.com>
2022-05-14 11:07:03 -0600

    adjusted swagger docs

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -3,13 +3,13 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Events API',
-    description: 'API for creating events and registering to attend them.'
+    description: 'API for creating events and registering to attend them.',
+    version: "1.0.3"
   },
   host: 'cse341-project02-mw.herokuapp.com',
   // host: 'localhost:8080',
   schemes: ['https'],
-  tags: [
-    {
+  tags: [{
       name: 'Users',
       description: 'Endpoints for Users'
     },
@@ -17,7 +17,24 @@ const doc = {
       name: 'Events',
       description: 'Endpoints for Events'
     }
-  ]
+  ],
+  // securityDefinitions: {
+  //   oAuth2: {
+  //     type: 'oauth2',
+  //     authorizationUrl: 'https://cse341-project02-mw.herokuapp.com/auth/google',
+  //     // flow: 'implicit',
+  //     // scopes: {
+  //     //   // read_pets: 'read your pets',
+  //     //   // write_pets: 'modify pets in your account'
+  //     // }
+  //   },
+  //   // api_key: {
+  //   //   type: "apiKey",
+  //   //   name:"api_key",
+  //   //   in: "header",
+  //   //   description: "An API key that hasn't been implemented"
+  //   // }
+  // }
 };
 
 const outputFile = './swagger.json';
